39fe08496e0ff72514e469d333e8881c9225a71c
ARTHUR VERDADEIRO <arthur.verdadeiro@etec.sp.gov.br>
2023-03-26 22:56:03 -0300

    first commit

---
+++ SmileSense/resources/views/loginpac.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pacientes - Smile Sense</title>
+    
+    <!--  STYLES  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" type="text/css" href="/SmileSense/resources/css/loginpac.css">
+
+    <!--  FONTS  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+</head>
+<body class="bg-dark">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('#cpf').mask('000.000.000-00');
+                });
+        </script>
+
+
+        <section class="vh-100 gradient-custom">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                            <div class="card-body p-5 text-center">
+
+                                <div class="mb-md-5 mt-md-4 pb-5">
+
+                                    <h2 class="fw-bold mb-2 text-uppercase">PACIENTES</h2>
+                                    <p class="text-white-50 mb-5">Por favor, insira seu CPF e sua senha!</p>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <label class="form-label" for="cpf">CPF</label>
+                                        <input type="text" name="cpf" id="cpf" class="form-control form-control-lg" />
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <label class="form-label" for="password">Senha</label>
+                                        <input type="password" name="password" id="password" class="form-control form-control-lg" />
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <label class="form-label" for="options">Selecione o consultório: </label>
+                                        <select class="form-select" aria-label="Default select example">
+                                            <option selected>Consultórios</option>
+                                            <option value="1">One</option>
+                                            <option value="2">Two</option>
+                                            <option value="3">Three</option>
+                                        </select
+                                    </div>
+
+                                    <p class="small mb-5 pb-lg-2">Esqueceu sua senha? <a class="text-white-50" href="#!">Clique aqui!</a></p>
+
+                                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</body>
+</html>
